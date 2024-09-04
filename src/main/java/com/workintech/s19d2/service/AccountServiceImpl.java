a915d07596248c789a47375dfccaabf190b6e03b
Mahmut Aktaş <mahmutaktas.m@gmail.com>
2024-09-04 22:01:59 +0300

    S19D3 completed

---
+++ src/main/java/com/workintech/s19d2/service/AccountServiceImpl.java
@@ -0,0 +1,35 @@
+package com.workintech.s19d2.service;
+
+import com.workintech.s19d2.entity.Account;
+import com.workintech.s19d2.repository.AccountRepository;
+import com.workintech.s19d2.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    public AccountRepository accountRepository;
+
+    @Autowired
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public List<Account> findAll() {
+        return accountRepository.findAll();
+    }
+
+    @Override
+    public Account save(Account account) {
+        return accountRepository.save(account);
+    }
+
+
+
+}
---
+++ src/main/java/com/workintech/s19d2/service/AuthenticationService.java
@@ -0,0 +1,71 @@
+package com.workintech.s19d2.service;
+
+import com.workintech.s19d2.entity.Member;
+import com.workintech.s19d2.entity.Role;
+import com.workintech.s19d2.repository.MemberRepository;
+import com.workintech.s19d2.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class AuthenticationService {
+
+    private static final String ROLE_USER = "USER";
+    private static final String ROLE_ADMIN = "ADMIN";
+    private MemberRepository memberRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthenticationService(MemberRepository memberRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.memberRepository = memberRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Member register(String email, String password) {
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+        if (memberOptional.isPresent()) {
+            throw new RuntimeException("User with given email already exist");
+        }
+        String encodedPassword = passwordEncoder.encode(password);
+
+        List<Role> roles = new ArrayList<>();
+
+        //addRoleUser(roles);
+        addRoleAdmin(roles);
+
+        Member member = new Member();
+        member.setEmail(email);
+        member.setPassword(encodedPassword);
+        member.setRoles(roles);
+        return memberRepository.save(member);
+    }
+
+    private void addRoleAdmin(List<Role> roles) {
+        Optional<Role> roleAdmin = roleRepository.findByAuthority(ROLE_ADMIN);
+        if (!roleAdmin.isPresent()){
+            Role roleUserEntity = new Role();
+            roleUserEntity.setAuthority(ROLE_ADMIN);
+            roles.add(roleRepository.save(roleUserEntity));
+        }
+        else {
+            roles.add(roleAdmin.get());
+        }
+    }
+
+    private void addRoleUser(List<Role> roles) {
+        Optional<Role> userRole = roleRepository.findByAuthority(ROLE_USER);
+        if (!userRole.isPresent()){
+            Role roleUserEntity = new Role();
+            roleUserEntity.setAuthority(ROLE_USER);
+            roles.add(roleRepository.save(roleUserEntity));
+        }
+        else {
+            roles.add(userRole.get());
+        }
+    }
+}
